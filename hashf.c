eeb37e853581bd8750d062aa7e3821af84165ae9
Gleb <gleb-skiba@mail.ru>
2017-12-04 02:24:59 +0300

    More const

--- hashf.c
+++ hashf.c
@@ -7,11 +7,11 @@ int32_t hash (str_t key,int dep) {
 // dep if parametr for change const in hash f
     int32_t mid = 0, p, m;
     switch (dep) {
-        case 1: p = 57; m = 1031; break;
-        case 2: p = 97; m = 3089; break;
-        case 3: p = 71; m = 2029; break;
-        case 4: p = 107; m = 4019; break;
-        default: p = 89; m = 6079; break;
+        case 1: p = 57; m = MAX_HASH_NODE; break;
+        case 2: p = 97; m = MAX_HASH_NODE; break;
+        case 3: p = 71; m = MAX_HASH_NODE; break;
+        case 4: p = 107; m = MAX_HASH_NODE; break;
+        default: p = 89; m = MAX_HASH_NODE; break;
     }
     for (uint32_t i = 0; i < key.size; i++){
         mid = (mid + p * ((int)(key.ptr[i]))) % m;
